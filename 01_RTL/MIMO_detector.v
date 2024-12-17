466c72e0a1548d61f42adac7d7134d0a1a91adb1
khoatm98 <khoatm98@gmail.com>
2024-12-17 23:18:29 +0800

    Dec 17

--- 01_RTL/MIMO_detector.v
+++ 01_RTL/MIMO_detector.v
@@ -1,9 +1,8 @@
 module MIMO_detector #(
-//parameter WIDTH = 32,
-parameter INT_W  = 2, // 1 sign bit and 2 integer bits
-parameter FRAC_W = 15,
+parameter INT_W  = 6, // 1 sign bit and 2 integer bits
+parameter FRAC_W = 10,
 parameter I_WIDTH = INT_W + FRAC_W,
-parameter O_WIDTH = 3
+parameter O_WIDTH = INT_W + FRAC_W
 )
 (input  wire                            Clk,
  input  wire                            Reset,
@@ -26,6 +25,7 @@ localparam S_INPUT_Y          = 5;
 localparam S_DFS_TRANVERSE_FW = 6;
 localparam S_DFS_TRANVERSE_BW = 7;
 localparam S_DFS_CHECK        = 8;
+localparam S_DFS_COMPUTE      = 9;
 /************/
 /* Reg/Wire */
 /************/
@@ -41,12 +41,14 @@ reg   [2:0]           curr_node_w     [3:0];   // 4 levels with 0-7 value
 reg   [2:0]           curr_distance_w [3:0];   // 4 levels with PED
 reg   [7:0]           pruning_w [3:0];         // 4 levels with 8 flag
 reg   [WIDTH*2:0]     best_distance_w      ;   // best_distance_w
+reg   [WIDTH*2:0]     best_candidate_w      ;   // best_distance_w
 
 reg   [2:0]           curr_level_r         ;   // 4 levels
 reg   [2:0]           curr_node_r     [3:0];   // 4 levels with 0-7
 reg   [2:0]           curr_distance_r [3:0];   // 4 levels with PED
-reg   [7:0]           pruning_r [3:0];         // 4 levels with 8 flag
+reg   [2:0]           pruning_r [3:0];         // 4 levels with 8 flag
 reg   [WIDTH*2:0]     best_distance_r      ;   // best_distance_r
+reg   [WIDTH*2:0]     best_candidate_r      ;   // best_distance_r
 
 wire                  can_tranverse_w;
 wire				  prune_w;
@@ -59,6 +61,7 @@ genvar i;
 assign input_R_valid_w = flagChannelorData && i_in_valid;
 assign input_Y_valid_w = !flagChannelorData && i_in_valid;
 assign o_in_ready      = curr_state == 1 || curr_state == 2 ||  curr_state == 3 || curr_state == 4 || curr_state == 5;
+
 always @(posedge Clk) begin
     R_buffer_r[3] <= input_R_valid_w ? InData        : R_buffer_r[3];
     R_buffer_r[2] <= input_R_valid_w ? R_buffer_r[3] : R_buffer_r[2];
@@ -82,13 +85,22 @@ always @(*) begin
     endcase
 end
 
+always @(*) begin
+    case(next_state)
+        S_DFS_CHECK         : best_distance_w = is_leaf_w && !prune_w ? curr_distance_r[curr_level_r] : best_distance_r;
+        S_INPUT_Y           : best_distance_w = {WIDTH*2{1'b1}};
+        default             : best_distance_w = best_distance_r;
+    endcase
+end
+
+
 
 generate
     for (i = 0; i < 4; i = i + 1) begin
         always @(*) begin
             if (i == curr_level_r) begin
                 case(next_state)
-                    S_DFS_TRANVERSE_FW  : pruning_w[curr_level_r] = {1'b1, pruning_r[curr_level_r][6:0]};
+                    S_DFS_TRANVERSE_FW  : pruning_w[curr_level_r] = pruning_r[curr_level_r] + 1;
                     default             : pruning_w[curr_level_r] = 0;
                 endcase
             end else begin
@@ -98,19 +110,96 @@ generate
     end
 endgenerate 
 
+/******************************/
+/*  S_DFS_COMPUTE             */
+/******************************/
+reg [WIDTH*2-1:0] Rs0;
+reg [WIDTH*2-1:0] Rs1;
+reg [WIDTH*2-1:0] Rs2;
+reg [WIDTH*2-1:0] Rs3;
 
-/******************************/
-/*  S_DFS_TRANVERSE_BW        */
-/******************************/
+reg [WIDTH*2-1:0] RS;    // 1 sign 3 bit integer 30 fractional bit
+
+reg [WIDTH*2-1:0] R0_r;
+reg [WIDTH*2-1:0] R1_r;
+reg [WIDTH*2-1:0] R2_r;
+reg [WIDTH*2-1:0] R3_r;
+
+reg [WIDTH*2-1:0] s_r;
+reg [WIDTH*2-1:0] y_hat_r;
+
+reg               RS_done_w;
+reg 			  compute_distance_done_w;
+reg 			  accum_done_w;
+
+complex_multiply complex_multiply_inst0(.i_clk  (Clk), 
+										.i_valid(compute_start_r),
+										.i_in_a (R0_r), 
+										.i_in_b (s_r),
+										.o_data (Rs0),
+										.o_valid()
+										)
+			
+complex_multiply complex_multiply_inst1(.i_clk  (Clk), 
+										.i_valid(compute_start_r),
+										.i_in_a (R1_r), 
+										.i_in_b (s_r),
+										.o_data (Rs1),
+										.o_valid()
+										)
+complex_multiply complex_multiply_inst2(.i_clk  (Clk), 
+										.i_valid(compute_start_r),
+										.i_in_a (R2_r), 
+										.i_in_b (s_r),
+										.o_data (Rs2),
+										.o_valid()
+										)
+
+complex_multiply complex_multiply_inst3(.i_clk  (Clk), 
+			                            .i_valid(compute_start_r),
+			                            .i_in_a (R3_r), 
+			                            .i_in_b (s_r),
+			                            .o_data (Rs3),
+			                            .o_valid(RS_done_w)
+			                           )
+
+accum accum_inst0(.i_clk  (Clk), 
+			      .i_valid(RS_done_w),
+			      .i_in_a (Rs0), 
+			      .i_in_b (Rs1),
+				  .i_in_c (Rs2), 
+			      .i_in_d (Rs3),
+			      .o_data (Rs),
+			      .o_valid(accum_done_w)
+			     )
+
+PED PED_inst(.i_clk  (Clk), 
+			 .i_valid(accum_done_w),
+			 .i_in_a (y_hat_r),         //  1 sign 1 bit integer 15 fractional bit
+			 .i_in_b (Rs),              //  1 sign 3 bit integer 30 fractional bit
+			 .o_data (distance_w),      //  1 sign 3 bit integer 30 fractional bit
+			 .o_valid(compute_distance_done_w)
+			)
+
+
+
 assign can_tranverse_w = &pruning_r[curr_level_r];
 
-always@ (*) begin
-    case(curr_state)
-        S_DFS_TRANVERSE_FW  : curr_distance_w[curr_level_r] = curr_distance_r[curr_level_r] + distance_w;
-        S_DFS_CHECK         : curr_distance_w[curr_level_r] = is_leaf_w || prune_w ? curr_distance_r[curr_level_r - 1] :;
-        default             : next_state = S_RST;  
-    endcase
-end
+generate
+    for (i = 0; i < 4; i = i + 1) begin
+		always@ (*) begin
+			if (i == curr_level_r) begin
+				case(curr_state)
+					S_DFS_TRANVERSE_FW  : curr_distance_w[i] = curr_distance_r[i - 1];
+					S_DFS_COMPUTE       : curr_distance_w[i] = compute_distance_done_w ? curr_distance_r[i] + distance_w : curr_distance_r[i];
+					default             : curr_distance_w[i] = curr_distance_r[i];
+				endcase
+			end else begin
+				curr_distance_w[i] = curr_distance_r[i];
+			end
+		end
+    end
+endgenerate 
 
 /******************************/
 /*  S_DFS_PRUNING        */
@@ -123,7 +212,7 @@ assign prune_w         = curr_distance_r[curr_level_r] >= best_distance_r;
 /*  S_DFS_TRANVERSE_FW        */
 /******************************/
 
-assign is_leaf_w = curr_level_r == 3;
+assign is_leaf_w = curr_level_r == 4;
     
 
 
@@ -139,7 +228,8 @@ always@ (*) begin
         S_INPUT_R2          : next_state = input_R_valid_w ? S_INPUT_R3 : S_INPUT_R2;  // Wait to get R vector
         S_INPUT_R3          : next_state = input_R_valid_w ? S_INPUT_Y  : S_INPUT_R3;  // Wait to get R vector
         S_INPUT_Y           : next_state = input_Y_valid_w ? S_DFS_TRANVERSE_BW : S_INPUT_Y;  // Wait to get first L vector
-        S_DFS_TRANVERSE_FW  : next_state = S_DFS_CHECK;
+        S_DFS_TRANVERSE_FW  : next_state = S_DFS_COMPUTE;
+		S_DFS_COMPUTE       : next_state = compute_distance_done_w ? S_DFS_CHECK : S_DFS_COMPUTE;
 		S_DFS_CHECK         : next_state = is_leaf_w || prune_w ? S_DFS_TRANVERSE_BW : S_DFS_TRANVERSE_FW;
         S_DFS_TRANVERSE_BW  : next_state = can_tranverse_w ? S_DFS_TRANVERSE_FW      : (curr_level_r == 0 ? S_OUTPUT : S_DFS_TRANVERSE_BW);
         S_OUTPUT            : next_state = S_RST; 
--- 01_RTL/complex_multiply.v
+++ 01_RTL/complex_multiply.v
@@ -1,63 +1,143 @@
 module complex_multiply #(
     //parameter WIDTH = 32,
-    parameter INT_W  = 2, // 1 sign bit and 2 integer bits
-    parameter FRAC_W = 15,
-    parameter I_WIDTH = INT_W + FRAC_W,)
+    parameter INT_W  = 6,
+    parameter FRAC_W = 10,
+    parameter WIDTH = INT_W + FRAC_W)
 (
-input signed  [I_WIDTH-1:0]   i_a_real,
-input signed  [I_WIDTH-1:0]   i_a_imag,
-input signed  [I_WIDTH-1:0]   i_b_real,
-input signed  [I_WIDTH-1:0]   i_b_imag,
-output signed [I_WIDTH*2-1:0] o_real,
-output signed [I_WIDTH*2-1:0] o_imag
+input  				 i_clk  ,
+input  				 i_valid,
+input  [WIDTH*2-1:0] i_in_a ,
+input  [WIDTH*2-1:0] i_in_b ,
+output [WIDTH*2-1:0] o_data ,
+output				 o_valid
 );
-wire signed [I_WIDTH-1:0] mult_ac;
-wire signed [I_WIDTH-1:0] mult_bd;
-wire signed [I_WIDTH-1:0] mult_ad;
-wire signed [I_WIDTH-1:0] mult_bc;
 
-multiplier mult_inst0(i_a_real,i_b_real,mult_ac);
-multiplier mult_inst1(i_a_imag,i_b_imag,mult_bd);
-multiplier mult_inst2(i_a_real,i_b_imag,mult_ad);
-multiplier mult_inst3(i_a_imag,i_b_real,mult_bc);
+function automatic [DATA_W-1:0] fx_mul;
+	input signed [WIDTH-1:0] i_data_a;
+	input signed [WIDTH-1:0] i_data_b;
 
-assign o_real = dout_ac - dout_bd;
-assign o_imag = dout_ad + dout_bc;
-    
+	reg   signed [WIDTH + 7:0]  tmp;
+	reg   signed [WIDTH - 1:0]  flip;
+	reg round;
+		
+	
+	begin
+        if (|i_data_b[FRAC_W-1 : 0]) begin // +- 1/sqrt(2)
+			tmp = {6{i_data_a[WIDTH-1]}, i_data_a, 1'b0} + {i_data_a,7'd0} + {2{i_data_a[WIDTH-1]}, i_data_a, 5'd0} + 
+				  {3{i_data_a[WIDTH-1]}, i_data_a, 4'd0} + {5{i_data_a[WIDTH-1]}, i_data_a, 2'd0};// + (i_data_a<<0);
+        end else begin // +- 1
+			tmp = {{i_data_a[WIDTH-1]}, i_data_a, 8'b0};
+        end
+		
+		flip = i_data_b[WIDTH-1] ? ~tmp[WIDTH + 7:8] + 1 : tmp[WIDTH + 7:8];
+		
+		fx_mul = flip[WIDTH*2-INT_W - 1 -:WIDTH];
+	end
+	
+endfunction
+
+reg o_valid_r;
+reg [WIDTH-1  :0] o_real_data_r;
+reg [WIDTH-1  :0] o_imag_data_r;
+
+wire signed [WIDTH-1  :0] real_a = i_in_a[WIDTH*2-1 :WIDTH];  // 1 sign 1 bit integer 15 fractional bit
+wire signed [WIDTH-1  :0] imag_a = i_in_a[WIDTH-1:0];         // 1 sign 1 bit integer 15 fractional bit
+wire signed [WIDTH-1  :0] real_b = i_in_b[WIDTH*2-1 :WIDTH];  // 1 sign 1 bit integer 15 fractional bit
+wire signed [WIDTH-1  :0] imag_b = i_in_b[WIDTH-1:0];         // 1 sign 1 bit integer 15 fractional bit
+
+wire signed [WIDTH*2-1:0] mult_ac = fx_mul(real_a, real_b);          // 2 sign 2 bit integer 30 fractional bit
+wire signed [WIDTH*2-1:0] mult_bd = fx_mul(real_a, real_b);          // 2 sign 2 bit integer 30 fractional bit
+wire signed [WIDTH*2-1:0] mult_ad = fx_mul(real_a, real_b);          // 2 sign 2 bit integer 30 fractional bit
+wire signed [WIDTH*2-1:0] mult_bc = fx_mul(real_a, real_b);          // 2 sign 2 bit integer 30 fractional bit
+
+always@ (posedge i_clk) begin
+	o_real_data_r <= i_valid ? mult_ac - mult_bd : o_real_data_r;  // 1 sign 2 bit integer 30 fractional bit 
+	o_imag_data_r <= i_valid ? mult_ad + mult_bc : o_imag_data_r;  // 1 sign 2 bit integer 30 fractional bit
+	o_valid_r     <= i_valid;
+end
+
+assign o_data = {o_real_data_r, o_imag_data_r};
+assign o_valid = o_valid_r;
+
 endmodule
 
-module multiplier(
-input signed [27:0] i_a,
-input signed [27:0] i_b,
-output reg signed [55:0] dout
+
+module accum#(     //parameter WIDTH = 32,
+	parameter INT_W  = 6,
+    parameter FRAC_W = 10,
+    parameter WIDTH = INT_W + FRAC_W)
+(
+input  				          i_clk  ,
+input  				          i_valid,
+input  [WIDTH*2-1:0]          i_in_a ,
+input  [WIDTH*2-1:0]          i_in_b ,
+input  [WIDTH*2-1:0]          i_in_c ,
+input  [WIDTH*2-1:0]          i_in_d ,
+output [WIDTH*2-1:0]          o_data ,
+output				          o_valid
 );
-reg [27:0] num1;
-reg [27:0] num2;
-wire [55:0] sum;
 
-always @(*)
-begin
-	if(din1 < 0)
-		num1[27:0] = ~(din1[27:0] -1'b1);
-	else
-		num1[27:0] = din1[27:0];
-		
-	if(din2 < 0)
-		num2[27:0] = ~(din2[27:0] - 1'b1);
-	else
-		num2[27:0] = din2[27:0];            
+
+wire signed [WIDTH-1:0] real_a = i_in_a[WIDTH*2-1: WIDTH]; 
+wire signed [WIDTH-1:0] imag_a = i_in_a[WIDTH-1 :0];
+wire signed [WIDTH-1:0] real_b = i_in_b[WIDTH*2-1: WIDTH]; 
+wire signed [WIDTH-1:0] imag_b = i_in_b[WIDTH-1 :0];
+
+wire signed [WIDTH-1:0] real_c = i_in_c[WIDTH*2-1: WIDTH]; 
+wire signed [WIDTH-1:0] imag_c = i_in_c[WIDTH-1 :0];
+wire signed [WIDTH-1:0] real_d = i_in_d[WIDTH*2-1: WIDTH]; 
+wire signed [WIDTH-1:0] imag_d = i_in_d[WIDTH-1 :0];
+
+wire signed [WIDTH+1:0] sum_real = {2{real_a[WIDTH-1]}, real_a} + {2{real_b[WIDTH-1]}, real_b} + {2{real_c[WIDTH-1]}, real_c} + {2{real_d[WIDTH-1]}, real_d};
+wire signed [WIDTH+1:0] sum_imag = {2{imag_a[WIDTH-1]}, imag_a} + {2{imag_b[WIDTH-1]}, imag_b} + {2{imag_c[WIDTH-1]}, imag_c} + {2{imag_d[WIDTH-1]}, imag_d};
+
+
+reg [WIDTH*2-1:0]       o_data_r;
+reg				        o_valid_r;
+
+
+always@ (posedge i_clk) begin
+	o_data_r      <= i_valid ? {sum_real[WIDTH-1:0], sum_imag[WIDTH-1:0]} : o_data_r;  // 1 sign 3 bit integer 30 fractional bit
+	o_valid_r     <= i_valid;
 end
 
-mul mul1(.din1(num1),.din2(num2),.dout(sum));
+assign o_data  = o_data_r;
+assign o_valid = o_valid_r;
 
-always @(*)
-begin
-	if((din1 < 0 && din2 < 0) || (din1 > 0 && din2 > 0))
-		dout[55:0] = sum[55:0];
-	else if((din1 < 0 && din2 > 0) || (din1 > 0 && din2 < 0))
-		dout[55:0] = ~sum[55:0] + 1'b1;
-	else
-		dout = 0;    
+endmodule
+
+module PED#(     //parameter WIDTH = 32,
+	parameter INT_W  = 6,
+    parameter FRAC_W = 10,
+    parameter WIDTH = INT_W + FRAC_W)
+(
+	input  i_clk  ,
+	input  i_valid,
+	input  i_in_a , // current distance
+	input  i_in_b ,
+	output o_data ,
+	output o_valid
+)
+reg [WIDTH*2-1:0]       o_data_r;
+reg	[1:0]               o_valid_r;
+
+wire signed [WIDTH-1:0] real_a = i_in_a[WIDTH*2-1: WIDTH]; 
+wire signed [WIDTH-1:0] imag_a = i_in_a[WIDTH-1 :0];
+
+wire signed [WIDTH-1:0] real_b = i_in_b[WIDTH*2-1: WIDTH]; 
+wire signed [WIDTH-1:0] imag_b = i_in_b[WIDTH-1 :0];
+
+wire signed [WIDTH:0] sum_real = {1{real_a[WIDTH-1]}, real_a} + {1{real_b[WIDTH-1]}, real_b};
+wire signed [WIDTH:0] sum_imag = {1{imag_a[WIDTH-1]}, imag_a} + {1{imag_b[WIDTH-1]}, imag_b};
+
+wire signed [WIDTH*2-1:0] sum_square = o_data_r[WIDTH*2-1:WIDTH] * o_data_r[WIDTH-1:0] ;
+
+always@ (posedge i_clk) begin
+	o_data_r      <= i_valid ? {sum_real[WIDTH-1:0], sum_imag[WIDTH-1:0]} : (o_valid_r[1] ? sum_square[DATA_W*2-INT_W - 1 -:DATA_W] : o_data_r);  // 1 sign 3 bit integer 30 fractional bit
+	o_valid_r     <= {o_valid_r[0], i_valid};
 end
+
+assign o_data  = o_data_r;
+assign o_valid = o_valid_r[1];
 
 endmodule
